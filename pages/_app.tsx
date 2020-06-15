c20adc94d9f19082f3e2bcb981c706e7d6ae7bdd
Klinton Ballecer <klinton.ballecer@gmail.com>
2020-06-15 21:50:27 +0800

    Add graphql endpoint fallback

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,10 @@ import { createClient, Provider } from 'urql';
 import { AppProps } from 'next/app';
 import fetch from 'isomorphic-unfetch';
 
-const client = createClient({ url: process.env.GRAPHQL_ENDPOINT, fetch });
+const client = createClient({
+  url: process.env.GRAPHQL_ENDPOINT || 'http://localhost:4000/api/graphql',
+  fetch
+});
 
 const App = ({ Component, pageProps }: AppProps) => {
     return (
